a54ed5c4b7d9f1da0d9c96c83339b873ef9fc5b9
qiushi <qiushi95@foxmail.com>
2018-07-17 21:08:14 +0800

    update views

--- opsGame/views.py
+++ opsGame/views.py
@@ -40,7 +40,7 @@ def fileDo():
 def testPing():
 
     # 传入inventory路径
-    ansible = MyRunner('/etc/ansible/hosts')
+    ansible = runner.ansibleRunner('/etc/ansible/hosts')
     # 获取服务器磁盘信息
     ansible.run('all', 'setup', "filter='ansible_mounts'")
     # 结果
@@ -51,5 +51,9 @@ def testPing():
     failed = result['failed']
     # 不可到达
     unreachable = result['unreachable']
+
+    print(succ)
+    print(failed)
+    print(unreachable)
 
     return "test ok!"
