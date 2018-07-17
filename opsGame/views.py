47eb6f8f721348146e18486fe179cfcd882ab975
qiushi <qiushi95@foxmail.com>
2018-07-17 20:59:17 +0800

    add a untested ansibleApi

--- opsGame/views.py
+++ opsGame/views.py
@@ -8,7 +8,11 @@ from sqlalchemy import and_
 from werkzeug.utils import redirect
 from ansible.inventory.manager import InventoryManager
 from ansible.playbook.play import Play
+from ansible.vars.manager import VariableManager
+from ansible.parsing.dataloader import DataLoader
 from ansible.plugins.callback import CallbackBase
+
+from ansibleApi import runner
 
 from opsGame import app, db
 # from opsGame.models import hostInfo
@@ -30,3 +34,22 @@ def cmd():
 @app.route('/ops/fileDo/', methods=['GET'])
 def fileDo():
     return render_template('FileDo.html')
+
+
+@app.route('/ops/testPing', methods=['GET'])
+def testPing():
+
+    # 传入inventory路径
+    ansible = MyRunner('/etc/ansible/hosts')
+    # 获取服务器磁盘信息
+    ansible.run('all', 'setup', "filter='ansible_mounts'")
+    # 结果
+    result = ansible.get_result()
+    # 成功
+    succ = result['success']
+    # 失败
+    failed = result['failed']
+    # 不可到达
+    unreachable = result['unreachable']
+
+    return "test ok!"
