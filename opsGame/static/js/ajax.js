1ee3d345c07e3059e7813d1f7722babcda7edc26
qiushi <qiushi95@foxmail.com>
2018-07-20 09:33:14 +0800

    add some templates, add some script

--- opsGame/static/js/ajax.js
+++ opsGame/static/js/ajax.js
@@ -7,12 +7,64 @@ $("button#sub").click(function () {
 	$.ajax({
 		type: 'post',
 		contentType: "application/json; charset=UTF-8",
-		url: "/command/",
+		url: "/Command/",
 		dataType: 'json',
 		data:JSON.stringify({
 				'host': 	    $("input#host").val(),
 				'module_name':  $("input#module_name").val(),
 				'module_args':	$("input#module_args").val(),
+			}),
+		success:function (data) {
+			if(data.status == "success"){
+				// alert(data.success);
+				$("#p2").append("<p><h5>SuccessHosts：</h5>");
+                for(var i = 0; i < data.successHosts.length; i++){
+                    $("#p2").append("<li>"+data.successHosts[i]+"</li>");
+
+                }
+                $("#p2").append("</p><br/>");
+                $("#p2").append("<p><h5>FailedsHosts：</h5>");
+                for(var i = 0; i < data.failedHosts.length; i++){
+                    $("#p2").append("<li>"+data.failedHosts[i]+"</li>");
+
+                }
+                $("#p2").append("</p><br/>");
+                $("#p2").append("<p><h5>UnreachableHosts：</h5>");
+                for(var i = 0; i < data.unreachableHosts.length; i++){
+                    $("#p2").append("<li>"+data.unreachableHosts[i]+"</li>");
+
+                }
+                $("#p2").append("</p><br/>");
+				$("#p2").append("<h5>HostDetails:</h5><br/>");
+                    // $("#p2").append(data.hosts+"<br/>");
+                    // $("#p2").append(data.hosts[0]['stdout']+"<br/>");
+                for(let jsondata in data.hosts){
+                    $("#p2").append("<p>"+jsondata+"===>>"+JSON.stringify(data.hosts[jsondata])+"</p><br/>");
+                }
+
+                $("#p2").append('================分割===================');
+
+			}else if(data.status == "fail"){
+				$("#p2").append('程序就没执行吧？老铁！');
+			}
+        }
+	});
+});
+/**
+ * Created by qius on 2018/7/19.
+ */
+
+$("button#fileDo").click(function () {
+
+	$.ajax({
+		type: 'post',
+		contentType: "application/json; charset=UTF-8",
+		url: "/FileDo/",
+		dataType: 'json',
+		data:JSON.stringify({
+				'host': $("input#host").val(),
+				'src':  $("input#src").val(),
+				'dest':	$("input#dest").val(),
 			}),
 		success:function (data) {
 			if(data.status == "success"){
--- opsGame/views.py
+++ opsGame/views.py
@@ -29,14 +29,62 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/cmd/', methods=['GET'])
-def cmd():
-    return "ok!路由测试成功"
-
-
-@app.route('/ops/fileDo/', methods=['GET'])
+@app.route('/FileDo/', methods=['GET', 'POST'])
 def fileDo():
-    return render_template('FileDo.html')
+    if request.method=="POST":
+        jsonData = request.get_json()
+        print(jsonData)
+        host = jsonData['host']
+        module_name = jsonData['src']
+        module_args = jsonData['dest']
+
+        ansible.run(host, 'copy', 'src='+module_name+' '+'dest='+module_args)
+        # 结果
+        result = ansible.get_result()
+        # 成功
+        succ = result['success']
+        # 失败
+        failed = result['failed']
+        # 不可到达
+        unreachable = result['unreachable']
+        # print(succ)
+        # print(failed)
+        # # print(unreachable)
+        data = {}
+        data['status'] = 'success'
+        data['successHosts'] = result['success'].keys()
+        data['failedHosts'] = result['failed'].keys()
+        data['unreachableHosts'] = result['unreachable'].keys()
+        data['hosts'] = {}
+        if result['success']:
+            for dict in result['success']:
+                data['hosts'][dict] = result['success'][dict]
+        if result['failed']:
+            for dict in result['failded']:
+                data['hosts'][dict] = result['failed'][dict]
+        if result['unreachable']:
+            for dict in result['unreachable']:
+                data['hosts'][dict] = result['unreachable'][dict]
+            # print(result['unreachable'][dict])
+        print(data)
+        return json.dumps(data)
+
+    return render_template('filedo.html')
+
+
+@app.route('/Monitor/', methods=['GET', 'POST'])
+def monitor():
+    return render_template('monitor.html')
+
+
+@app.route('/Playbook/', methods=['GET', 'POST'])
+def playbook():
+    return render_template('playbook.html')
+
+
+@app.route('/Shell/', methods=['GET', 'POST'])
+def shell():
+    return render_template('shell.html')
 
 
 '''
@@ -49,12 +97,12 @@ def fileDo():
     data[host]：机器的具体状态列表，key中保存stdout_lines
     
 '''
-@app.route('/command/', methods=["GET"])
+@app.route('/Command/', methods=["GET"])
 def commandPage():
     return render_template('command.html')
 
 
-@app.route('/command/', methods=["GET", "POST"])
+@app.route('/Command/', methods=["GET", "POST"])
 def commandRun():
     if request.method=="POST":
         jsonData = request.get_json()
